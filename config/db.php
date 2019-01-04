59996e4c51d0451e8f56230470a625287ad880d8
samgo01 <31669606+samgo01@users.noreply.github.com>
2019-01-04 10:36:56 -0600

    Second commit

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'id3505016_agenda');//Usuario de tu base de datos
+define('DB_PASS', 'carlos');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'id3505016_agenda');//Nombre de la base de datos
+?>
