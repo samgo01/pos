a5c1df5d4a2e75b6431bb55b9da5c2e16f4489a9
samgo01 <31669606+samgo01@users.noreply.github.com>
2019-01-04 10:38:25 -0600

    Three Commit

---
+++ js/productos.js
@@ -0,0 +1,75 @@
+		$(document).ready(function(){
+			load(1);
+		});
+
+		function load(page){
+			var q= $("#q").val();
+			$("#loader").fadeIn('slow');
+			$.ajax({
+				url:'./ajax/buscar_productos.php?action=ajax&page='+page+'&q='+q,
+				 beforeSend: function(objeto){
+				 $('#loader').html('<img src="./img/ajax-loader.gif"> Cargando...');
+			  },
+				success:function(data){
+					$(".outer_div").html(data).fadeIn('slow');
+					$('#loader').html('');
+					
+				}
+			})
+		}
+
+	
+		
+			function eliminar (id)
+		{
+			var q= $("#q").val();
+		if (confirm("Realmente deseas eliminar el producto")){	
+		$.ajax({
+        type: "GET",
+        url: "./ajax/buscar_productos.php",
+        data: "id="+id,"q":q,
+		 beforeSend: function(objeto){
+			$("#resultados").html("Mensaje: Cargando...");
+		  },
+        success: function(datos){
+		$("#resultados").html(datos);
+		load(1);
+		}
+			});
+		}
+		}
+		
+	
+
+$( "#editar_producto" ).submit(function( event ) {
+  $('#actualizar_datos').attr("disabled", true);
+  
+ var parametros = $(this).serialize();
+	 $.ajax({
+			type: "POST",
+			url: "ajax/editar_producto.php",
+			data: parametros,
+			 beforeSend: function(objeto){
+				$("#resultados_ajax2").html("Mensaje: Cargando...");
+			  },
+			success: function(datos){
+			$("#resultados_ajax2").html(datos);
+			$('#actualizar_datos').attr("disabled", false);
+			load(1);
+		  }
+	});
+  event.preventDefault();
+})
+
+	function obtener_datos(id){
+			var codigo_producto = $("#codigo_producto"+id).val();
+			var nombre_producto = $("#nombre_producto"+id).val();
+			var estado = $("#estado"+id).val();
+			var precio_producto = $("#precio_producto"+id).val();
+			$("#mod_id").val(id);
+			$("#mod_codigo").val(codigo_producto);
+			$("#mod_nombre").val(nombre_producto);
+			$("#mod_precio").val(precio_producto);
+			$("#mod_estado").val(estado);
+		}
+		
